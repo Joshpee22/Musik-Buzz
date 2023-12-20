166f197ee24ed771c5bf324fea54c2df7539da14
Rita2024 <ritaejezie2@gmail.com>
2023-12-20 12:21:25 +0100

    music playing

--- Music/data.js
+++ Music/data.js
@@ -1,49 +1,49 @@
 let songs = [
     {
         name: 'Asake-Lonely-At-The-Top-Remix-Ft-Her',
-        path: 'Music/music/Asake-Lonely-At-The-Top-Remix-Ft-Her.mp3',
+        path: 'audio/Asake-Lonely-At-The-Top-Remix-Ft-Her.mp3',
         artist: 'Asake',
         cover: 'images/Asake lonely at the top remix.jpg'
     },
     {
         name: 'Berri-Tiga-Egopiano-Ft.-Tekno',
-        path: 'Music/music/Berri-Tiga-Egopiano-Ft.-Tekno.mp3',
+        path: 'audio/Berri-Tiga-Egopiano-Ft.-Tekno.mp3',
         artist: 'Berri-Tiga',
         cover: 'images/egopiano ft tekno.jpg'
     },
     {
         name: 'Breezy-Galaxy-Bounce-ft-Ruger-Boy',
-        path: 'Music/music/Breezy-Galaxy-Bounce-ft-Ruger-Boy.mp3',
+        path: 'audio/Breezy-Galaxy-Bounce-ft-Ruger-Boy.mp3',
         artist: 'Breezy-Galaxy',
         cover: 'images/Bounce ft Ruger Boy.jpg'
     },
     {
         name: 'Burna-Boy-Way-Too-Big',
-        path: 'Music/music/Burna-Boy-Way-Too-Big.mp3',
+        path: 'audio/Burna-Boy-Way-Too-Big.mp3',
         artist: 'Burna-Boy',
         cover: 'images/Burna.jpg'
     },
     {
         name: 'Salle-Ojuju-Ft.-Commissioner-DJ-Wysei',
-        path: 'Music/music/Salle-Ojuju-Ft.-Commissioner-DJ-Wysei.mp3',
+        path: 'audio/Salle-Ojuju-Ft.-Commissioner-DJ-Wysei.mp3',
         artist: 'Salle-Ft.-Commissioner-DJ-Wysei',
         cover: 'images/Salle-Ojuju-Ft.-Commissioner-DJ-Wysei.mp3.jpg'
     },
     {
         name: 'Salle-Sipe-Nwa',
-        path: 'Music/music/Salle-Sipe-Nwa-Gen-Z-Version-Zacknation.net_.mp3',
+        path: 'audio/Salle-Sipe-Nwa-Gen-Z-Version-Zacknation.net_.mp3',
         artist: 'Salle',
         cover: 'images/Salle-Ojuju-Ft.-Commissioner-DJ-Wysei.mp3.jpg'
     },
     {
         name: 'Shallipopi-Cast-Ft.-Odumodublvck',
-        path: 'Music/music/Shallipopi-Cast-Ft.-Odumodublvck.mp3',
+        path: 'audio/Shallipopi-Cast-Ft.-Odumodublvck.mp3',
         artist: 'Shallipopi-Ft-Odumodublvck',
         cover: 'images/shallipopi.jpg'
     },
     {
         name: 'SMADA EH feat Poco Lee Smeez  D3an',
-        path: 'Music/music/SMADA EH feat Poco Lee Smeez  D3an.mp3',
+        path: 'audio/SMADA EH feat Poco Lee Smeez  D3an.mp3',
         artist: 'Smada Eh Ft Poco Lee',
         cover: 'images/smada eh ft poco lee.jpg'
     },
